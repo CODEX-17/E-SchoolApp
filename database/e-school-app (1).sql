1036dc67f8119023f6598f8dd62b24a9cfd70a41
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2024-08-09 14:04:46 +0800

    final with db

--- database/e-school-app (1).sql
+++ database/e-school-app (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 01, 2024 at 04:15 AM
+-- Generation Time: Aug 09, 2024 at 08:03 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
